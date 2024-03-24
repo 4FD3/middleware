371aff99155ef0451e4fe887785c823094dd30d5
Parteek Dheri <git@dheri.ca>
2024-03-24 13:46:58 -0400

    cors for .google.com

--- src/main/java/ca/dheri/digitalizingreceipts/config/WebConfig.java
+++ src/main/java/ca/dheri/digitalizingreceipts/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:3000", "https://*.dheri.ca", "https://4fd3.dheri.ca")
+                .allowedOriginPatterns("http://localhost:3000", "https://*.dheri.ca", "https://accounts.google.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
