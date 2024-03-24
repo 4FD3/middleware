86a217bee4acf5c9c63c25daed8c0930df34bd48
Parteek Dheri <git@dheri.ca>
2024-03-24 01:21:01 -0400

    allowedOriginPatterns

--- src/main/java/ca/dheri/digitalizingreceipts/config/WebConfig.java
+++ src/main/java/ca/dheri/digitalizingreceipts/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://*.dheri.ca", "https://4fd3.dheri.ca")
+                .allowedOriginPatterns("http://localhost:3000", "https://*.dheri.ca", "https://4fd3.dheri.ca")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true);
     }
